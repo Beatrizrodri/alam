295ecaecbab7cf0e7b56a8533b4b85399110d585
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2023-01-24 19:32:59 -0300

    add DefaultButton Component

---
+++ src/components/Buttons/DefaultButton/Index.jsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.scss';
+
+function DefaultButton({ text, type }) {
+  const colors = {
+    danger: '#FF8282',
+    success: '#A1C5A3',
+    warning: '#F1987A',
+    secondary: '#DFC1A8',
+    info: '#335360',
+  };
+
+  return (
+    <button
+      style={{
+        backgroundColor: colors[type],
+        color: type === 'info' ? 'white' : 'black',
+      }}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default DefaultButton;
